d258d3ad344ba8db8757314eb46ba4cc6ecb09b9
Gustavo Rallenosn Gonçalves Da Silva <Rallenson900@gmail.com>
2023-10-26 17:54:52 -0300

    criando fieald de login

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,50 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.purple,
         ),
-        home: Placeholder());
+        home: WidgetLogin());
+  }
+}
+
+class WidgetLogin extends StatefulWidget {
+  const WidgetLogin({super.key});
+  @override
+  _WidgetLoginState createState() => _WidgetLoginState();
+}
+
+class _WidgetLoginState extends State<WidgetLogin> {
+  final TextStyle estilo = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+  @override
+  Widget build(BuildContext context) {
+    final emailFielld = TextField(
+      obscureText: false,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Email",
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+    final passFielld = TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Email",
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+    final buttonLogin = ButtonTheme(
+      minWidth: MediaQuery.of(context).size.width,
+      padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+      child: ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(Colors.orange),
+          elevation: MaterialStateProperty.all<double?>(30.0),
+        ),
+        child: Text("Login"),
+        onPressed: () {},
+      ),
+    );
+
+
+    return Placeholder();
   }
 }
